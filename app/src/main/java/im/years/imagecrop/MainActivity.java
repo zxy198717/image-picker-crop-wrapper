20a4230cafb753334eb0a4d81df742e1b074e7b6
Alvin Zeng <alvin.zeng@augmentum.com>
2015-11-06 10:49:02 +0800

    init

---
+++ app/src/main/java/im/years/imagecrop/MainActivity.java
@@ -0,0 +1,104 @@
+package im.years.imagecrop;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.kbeanie.imagechooser.api.ChosenImage;
+
+import java.io.File;
+
+import im.years.imagepicker.ImagePickerManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImagePickerManager mImagePickerManager;
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        mImagePickerManager = new ImagePickerManager(this);
+    }
+
+
+    public void PickImage(View v) {
+        mImagePickerManager.pickImage(true ,new ImagePickerManager.ImagePickerListener() {
+            @Override
+            public void onImageChosen(ChosenImage image) {
+
+                final Uri source = Uri.parse(new File(image
+                        .getFileThumbnailSmall()).toString());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageView.setImageURI(source);
+                    }
+                });
+
+
+
+                Log.e("dddddd", image.getFilePathOriginal());
+            }
+
+            @Override
+            public void onError(String reason) {
+                Log.e("dddddd", reason);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mImagePickerManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ imagepicker/src/main/java/im/years/imagepicker/ImagePickerManager.java
@@ -0,0 +1,223 @@
+package im.years.imagepicker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.kbeanie.imagechooser.api.ChooserType;
+import com.kbeanie.imagechooser.api.ChosenImage;
+import com.kbeanie.imagechooser.api.ImageChooserListener;
+import com.kbeanie.imagechooser.api.ImageChooserManager;
+import com.soundcloud.android.crop.Crop;
+
+import java.io.File;
+
+/**
+ * Created by alvinzeng on 11/6/15.
+ */
+public class ImagePickerManager {
+
+    public interface ImagePickerListener {
+        /**
+         * When the processing is complete, you will receive this callback with
+         * {@link ChosenImage}
+         *
+         * @param image
+         */
+        public void onImageChosen(ChosenImage image);
+
+        /**
+         * Handle any error conditions if at all, when you receieve this callback
+         *
+         * @param reason
+         */
+        public void onError(String reason);
+    }
+
+    protected Activity activity;
+    protected android.app.Fragment appFragment;
+
+    private ChosenImage chosenedImage;
+    private ImagePickerListener imageChooserListener;
+    private ImageChooserManager imageChooserManager;
+
+    private ImagePickerManager(){};
+
+    public ImagePickerManager(Activity activity) {
+        this.activity = activity;
+    }
+
+    public ImagePickerManager(android.app.Fragment fragment) {
+        this.appFragment = fragment;
+    }
+
+    public void pickImage(ImagePickerListener l) {
+        this.pickImage(false, l);
+    }
+
+    public void pickImage(final boolean crop, final ImagePickerListener l) {
+        new ImagePickerActionSheet(getActivity(), new ImagePickerActionSheet.ImagePickerActionSheetListener() {
+            @Override
+            public void onItemSelected(int index) {
+                switch (index) {
+                    case 0:
+                        takePicture(crop, l);
+                        break;
+                    case 1:
+                        chooseImage(crop, l);
+                        break;
+                }
+            }
+        });
+    }
+
+    public void takePicture(final boolean crop, final ImagePickerListener l) {
+
+        imageChooserListener = l;
+
+        if(activity != null) {
+            imageChooserManager = new ImageChooserManager(activity,
+                    ChooserType.REQUEST_CAPTURE_PICTURE, true);
+        } else {
+            imageChooserManager = new ImageChooserManager(appFragment,
+                    ChooserType.REQUEST_CAPTURE_PICTURE, true);
+        }
+
+        imageChooserManager.setImageChooserListener(new ImageChooserListener() {
+            @Override
+            public void onImageChosen(ChosenImage chosenImage) {
+                if (crop) {
+                    Uri source = Uri.fromFile(new File(chosenImage
+                            .getFilePathOriginal()));
+                    Uri destination = Uri.fromFile(new File(chosenImage
+                            .getFileThumbnailSmall()));
+
+                    final Crop cropIns = Crop.of(source, destination).asSquare();
+                    if(activity != null) {
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                cropIns.start(activity);
+                            }
+                        });
+
+
+                    } else {
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                cropIns.start(appFragment.getActivity(), appFragment);
+                            }
+                        });
+
+                    }
+
+                    chosenedImage = chosenImage;
+                } else {
+                    l.onImageChosen(chosenImage);
+                }
+            }
+
+            @Override
+            public void onError(String s) {
+                l.onError(s);
+            }
+        });
+        try {
+            imageChooserManager.choose();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void chooseImage(final boolean crop, final ImagePickerListener l) {
+
+        imageChooserListener = l;
+
+        if(activity != null) {
+            imageChooserManager = new ImageChooserManager(activity,
+                    ChooserType.REQUEST_PICK_PICTURE, true);
+        } else {
+            imageChooserManager = new ImageChooserManager(appFragment,
+                    ChooserType.REQUEST_PICK_PICTURE, true);
+        }
+
+        imageChooserManager.setImageChooserListener(new ImageChooserListener() {
+            @Override
+            public void onImageChosen(ChosenImage chosenImage) {
+                if (crop) {
+
+                    Uri source = Uri.fromFile(new File(chosenImage
+                            .getFilePathOriginal()));
+                    Uri destination = Uri.fromFile(new File(chosenImage
+                            .getFileThumbnailSmall()));
+
+                    final Crop cropIns = Crop.of(source, destination).asSquare();
+                    if(activity != null) {
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                cropIns.start(activity);
+                            }
+                        });
+
+
+                    } else {
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                cropIns.start(appFragment.getActivity(), appFragment);
+                            }
+                        });
+
+                    }
+                } else {
+                    l.onImageChosen(chosenImage);
+                }
+
+                chosenedImage = chosenImage;
+            }
+
+            @Override
+            public void onError(String s) {
+                l.onError(s);
+            }
+        });
+        try {
+            imageChooserManager.choose();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent result){
+        if (resultCode == Activity.RESULT_OK) {
+
+            if (requestCode == Crop.REQUEST_CROP) {
+                imageChooserListener.onImageChosen(chosenedImage);
+            } else {
+                imageChooserManager.submit(requestCode, result);
+            }
+        }
+    }
+
+    protected Activity getActivity() {
+        if(activity != null) {
+            return activity;
+        }
+
+        if(appFragment != null) {
+            return appFragment.getActivity();
+        }
+
+        return  null;
+    }
+}
